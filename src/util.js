d51700ca3e5fcb1afc2cd427bd2f170978d3a028
Brian Kimball <bkimball@bullhorn.com>
2016-01-14 20:28:07 -0500

    Merged with latest for original repo

---
+++ src/util.js
@@ -0,0 +1,9 @@
+export function deepTraverse(obj, func) {
+  for (var i in obj) {
+    func.apply(this, [i, obj[i], obj]);
+    if (obj[i] !== null && typeof (obj[i]) == 'object') {
+      deepTraverse(obj[i], func);
+    }
+  }
+}
+;
